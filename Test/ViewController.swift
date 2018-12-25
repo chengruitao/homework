9f2c05613909038421834c4385c308987abe8e7d
aiait <aiait@zhangyonghui.local>
2018-12-25 13:20:20 +0800

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by aiait on 2018/12/25.
+//  Copyright © 2018 aiait.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
